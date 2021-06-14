425ee2053c914b62aaccc1717fe570cfff320871
reiy24 <rei.yatsuhashi@gmail.com>
2021-06-14 15:10:53 +0900

    Update naming convention

--- R/get_data.R
+++ R/get_data.R
@@ -13,24 +13,24 @@ clean_jcdf <- function(path){
   pref_raw <- st_read(path)
   
   # filter out water surfaces and extraneous port data
-  pref_name <- pref_raw %>% 
+  pref <- pref_raw %>% 
       filter(nagasaki_raw$HCODE != 8154, )  %>%
       group_by(PREF, CITY, KIHON1) %>% 
       summarize(geometry = st_union(geometry), JINKO = sum(JINKO))
   
   # reformatting the types of the JCDF data
-  pref_name$PREF <- as.numeric(pref_name$PREF)
-  pref_name$CITY <- as.numeric(pref_name$CITY)
+  pref$PREF <- as.numeric(pref$PREF)
+  pref$CITY <- as.numeric(pref$CITY)
   
   # creates municipality codes, of the form XXYYY
-  pref_name$REGION <- (1000*pref_name$PREF + pref_name$CITY) 
+  pref$REGION <- (1000*pref$PREF + pref$CITY) 
   
-  pref_name <- pref_name[, -c(1, 2)]
-  names(pref_name) <- c("town", "geometry", "pop", "code")
+  pref <- pref[, -c(1, 2)]
+  names(pref) <- c("town", "geometry", "pop", "code")
   
   # deleting redundant municipality-wide shapes
-  pref_name <- pref_name[which(pref_name$town != "0000"), ]
+  pref <- pref[which(pref$town != "0000"), ]
   
-  return(pref_name)
+  return(pref)
   
 }
