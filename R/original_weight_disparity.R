f346bcf784ebf3a76cdc8a5a4a126edd5af7801c
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2021-06-18 20:25:03 +0900

    added other paramters. Not yet HH

--- R/original_weight_disparity.R
+++ R/original_weight_disparity.R
@@ -1,19 +1,38 @@
 #' Calculate Original Ippyo-no-kakusa (maximum weight disparity in voter power)
 #'
 #' @param pref an sf object with cleaned, collated version of the census data
+#' @param parameter character vector of parameter (`max_to_min`, `Gini`, `LH`, and `HH`).
 #'
 #' @return a vector with the ippyo no kakusa for the oiginal plan
 #'
 #' @concept calculate
 #'
 
-original_weight_disparity <- function(pref){
+original_weight_disparity <- function(pref, parameter){
 
   orig <- pref %>%
     group_by(cd) %>%
     summarise(pop=sum(pop))
 
-  original_weight_disparity <- max(orig$pop)/min(orig$pop)
+  score <- NA
 
-  return(original_weight_disparity)
+  if(parameter == "max_to_min"){
+
+    score <- max(orig$pop)/min(orig$pop)
+
+  }else if(parameter == "Gini"){
+    score <- reldist::gini(x = orig$pop,
+                           weights = rep(1, nrow(orig)))
+
+  }else if(parameter == "LH"){
+    for (i in 1:nrow(orig)){
+      score <-  (abs((orig$pop[i] / sum(orig$pop)) - (1 / nrow(orig)))) / 2
+    }
+
+  }else if(parameter == "HH"){
+    # To be fixed
+    score <- NA
+  }
+
+  return(score)
 }
