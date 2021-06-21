c4f964517664c3e61a43de9c1f907620f35976d3
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2021-06-21 15:46:13 +0900

    reflect Rei's suggestions

--- R/clean_jcdf.R
+++ R/clean_jcdf.R
@@ -7,15 +7,20 @@
 #' @concept getdata
 #'
 
-clean_census <- function(path) {
+clean_jcdf <- function(path) {
 
-  pref <- NA
+  pref_raw <- sf::st_read(path)
+  pref_raw <- sf::st_make_valid(pref_raw)
 
   # filter out water surfaces and extraneous port data
+  pref <- NA
   pref <- path %>%
     dplyr::filter(path$KIHON1 != "0000" & path$HCODE != 8154, )  %>%
-    dplyr::group_by(PREF, CITY, KIHON1, CITY_NAME, S_NAME) %>%
-    dplyr::summarize(geometry = sf::st_union(geometry), JINKO = sum(JINKO)) %>%
+    dplyr::group_by(PREF, CITY, KIHON1, CITY_NAME) %>%
+    # make smallest geopolitical subdivision to level 2
+    dplyr::slice(1) %>%
+    dplyr::summarize(geometry = sf::st_union(geometry), JINKO = sum(JINKO), S_NAME) %>%
+    # make 5 digit municipality code
     dplyr::mutate(code = 1000*as.numeric(PREF) + as.numeric(CITY)) %>%
     dplyr::select(code, CITY_NAME, S_NAME, KIHON1, JINKO, geometry)
 
