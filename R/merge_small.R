2a4b18c918776038ed182fdc50777a62ab2c09de
reiy24 <rei.yatsuhashi@gmail.com>
2021-06-27 17:02:18 +0900

    Delete extraneous materials

--- R/merge_small.R
+++ R/merge_small.R
@@ -73,15 +73,4 @@ merge_small <- function(pref, split_codes = NULL, intact_codes = NULL){
   }
   return(bound)
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+
