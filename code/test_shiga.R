489de496055e1e6368b481c9f88fdcd4947f8690
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2021-06-29 01:03:17 +0900

    set relative path

--- code/test_shiga.R
+++ code/test_shiga.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 
 # pull functions from jcdf
-setwd("R")
+# set working directory to the function folder
+setwd("../R")
 files.sources = list.files()
 sapply(files.sources, source)
+# set working directory back to `jcdf`
+setwd("..")
 
 # Clean data
 pref_raw <- download_shp(25)
-
 pref <- clean_jcdf(pref_raw = pref_raw)
 
 total <- download_2020_census(type = "total")
