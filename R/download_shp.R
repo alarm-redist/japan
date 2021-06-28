5ba71a4dda0ab06fbadaf24e5835c5edf5a04a55
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2021-06-29 00:54:08 +0900

    revert to what it initially were (sorry, my mistake!)

--- R/download_shp.R
+++ R/download_shp.R
@@ -15,25 +15,25 @@ download_shp <- function(pref_code){
   pref_code <- sprintf('%02d', pref_code)
 
   # check if data/ folder exists in working directory
-  if(!file.exists("~/data")){
+  if(!file.exists("data")){
 
     # if not, create data folder
-    dir.create("~/data")
+    dir.create("data")
 
   }
 
   # download the files from the Japanese Government Statistics Portal (e-Stat)
   download.file(paste('https://www.e-stat.go.jp/gis/statmap-search/data?dlserveyId=A002005212015&code=',
-                      as.character(pref_code), '&coordSys=1&format=shape&downloadType=5', sep = ''), '~/data/shp_data.zip')
+                      as.character(pref_code), '&coordSys=1&format=shape&downloadType=5', sep = ''), 'data/shp_data.zip')
 
   # unzip the downloaded zip file
-  unzip("~/data/shp_data.zip", exdir = "~/data")
+  unzip("data/shp_data.zip", exdir = "data")
 
   # remove the zip file after zip is decompressed
-  file.remove("~/data/shp_data.zip")
+  file.remove("data/shp_data.zip")
 
   # return the shp file
-  pref_raw <- sf::st_read(paste("~/data/h27ka", as.character(pref_code), '.shp', sep = ''))
+  pref_raw <- sf::st_read(paste("data/h27ka", as.character(pref_code), '.shp', sep = ''))
   return(pref_raw)
 
 }
