2c42a22999b23a337561b087fb6557438a2620b2
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-06-30 16:08:57 +0900

    estimate 2020 pop national

--- code/34_test_hiroshima.R
+++ code/34_test_hiroshima.R
@@ -9,22 +9,26 @@ sapply(files.sources, source)
 # set working directory back to `jcdf`
 setwd("..")
 
-#-------- Clean data -----------#
+#-------- Clean data (2015 Census)-----------#
 # Clean data
 pref_raw <- download_shp(34) #34:Hiroshima
 pref <- clean_jcdf(pref_raw = pref_raw)
+#JINKO in pref includes foreigners too -> calculate Japanese population
+age_pops <- download_pop_demographics(34) #first download data on pop by age group
+pref <- calc_kokumin(pref, age_pops)
 
-#2020 census
+#-------- Update population (Estimate based on 2020 Census)-----------#
+# Download 2020 census
 total <- download_2020_census(type = "total")
 foreigner <- download_2020_census(type = "foreigner")
-#clean 2020 census
+# Clean 2020 census
 census2020 <- clean_2020_census(total = total, foreigner = foreigner)
 
 #check to see pop ranking by mun
 census2020 %>%
   filter(code > 34200 & code < 35000) %>% #excluding 34100 Hiroshima City
   arrange(desc(pop_national))
-#Excluding Hiroshima City, Top 4 is
+#Excluding Hiroshima City, Top 4 Muns
 #34207: 福山 (currently NOT split; pre-gappei: 207 福山 481 内海 482沼隈 501神辺 524新市)
 #34202: 呉 (currently NOT split; pre-gappei: 202呉 311音戸 312倉橋 313下蒲刈 324蒲刈 423安浦 424川尻 425豊浜 426豊)
 #34212: 東広島 (currently split; pre-gappei: 212東広島 402黒瀬 405福富 406豊栄 408河内 422安芸津)
@@ -35,9 +39,12 @@ census2020 %>%
 
 #Estimate 2020 Pop at the 小地域-level (pref is currently based on 2015 Census)
 #*Note: when splitting the 4 municipalities based on the pre-gappei boundaries,
-#*the poplation data will be based on the 2015 Census
+#*the population data will be based on the 2015 Census
 #*this inconsistency will be resolved once we get access to the 2020 Census
-
+pop <- estimate_2020_pop(pref, census2020)
+pop <- as.data.frame(pop)
+pref <- bind_cols(pref, pop)
+#The column "pop" in "pref" now indicates the "estimated number of Japanese nationals"
 
 #-------- SMC: 0 split -----------#
 
