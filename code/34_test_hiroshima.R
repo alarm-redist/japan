725a216ccd788279e369c5f11e3cfc4863331036
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-06-30 14:58:05 +0900

    cleaning data

--- code/34_test_hiroshima.R
+++ code/34_test_hiroshima.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# ----------- set up -------------#
 # pull functions from jcdf
 # set working directory to the function folder
 setwd("R")
@@ -8,15 +9,29 @@ sapply(files.sources, source)
 # set working directory back to `jcdf`
 setwd("..")
 
+#-------- Clean data -----------#
 # Clean data
 pref_raw <- download_shp(34) #34:Hiroshima
 pref <- clean_jcdf(pref_raw = pref_raw)
 
-
+#2020 census
 total <- download_2020_census(type = "total")
 foreigner <- download_2020_census(type = "foreigner")
-
+#clean 2020 census
 census2020 <- clean_2020_census(total = total, foreigner = foreigner)
+
+#check to see pop ranking by mun
+census2020 %>%
+  filter(code > 34200 & code < 35000) %>% #excluding 34100 Hiroshima City
+  arrange(desc(pop_national))
+#Excluding Hiroshima City, Top 4 is
+#34207: 福山 (currently NOT split; pre-gappei: 207 福山 481 内海 482沼隈 501神辺 524新市)
+#34202: 呉 (currently NOT split; pre-gappei: 202呉 311音戸 312倉橋 313下蒲刈 324蒲刈 423安浦 424川尻 425豊浜 426豊)
+#34212: 東広島 (currently split; pre-gappei: 212東広島 402黒瀬 405福富 406豊栄 408河内 422安芸津)
+#34205: 尾道(currently split; pre-gappei: 205尾道 206因島 430瀬戸田 441御調 444向島)
+#Since 4 municipalities (excluding Hiroshima City, a seireishiteitoshi) are currently split,
+#the 1st ~ 4th largest municipalities will be split.
+#江田島市 and 三原市, which are currently split, will no longer be split.
 
 merged <- pref %>%
   group_by(code, CITY_NAME) %>%
@@ -27,3 +42,10 @@ check <- merged %>%
   ggplot() +
   geom_sf(fill = "red")
 
+#-------- SMC: 0 split -----------#
+
+#-------- SMC: 1 split -----------#
+
+#######enumeration##################
+
+#######save as RData###############
