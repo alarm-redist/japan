55234810be98c5f70ce7844ff8fde2412354ce36
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-07-01 11:50:30 +0900

    toyama -> pref

--- R/reflect_old_boundaries.R
+++ R/reflect_old_boundaries.R
@@ -20,7 +20,7 @@
 #2. Download the old shapefile that reflect the boundaries as of 2000 using download_old_shp().
 #   Ex: toyama_old_boundary <- download_old_shp(16)
 #   equivalent to : old_boundary <- sf::st_read("data/N03-001001_16-g_AdministrativeBoundary.shp")
-#3. Download the 2015 Census data that shows the population as of 2015 per each old municipality 
+#3. Download the 2015 Census data that shows the population as of 2015 per each old municipality
 #   Ex: toyama_2015pop_old <- download_2015pop_old(16)
 #   equivalent to : pop_by_old_boundary <- read.csv("data/001_16.csv", fileEncoding = "shift-jis")
 #4.Specify the codes of the old municipalities that presently belong to a *single* municipality.
@@ -32,28 +32,28 @@
 reflect_old_boundaries <- function(pref, old_boundary, pop_by_old_boundary, old_code, new_code){
   #Match CRS
   sf::st_crs(old_boundary) <- sf::st_crs(pref)
-  
+
   #filter out the municipalities that will not be split
   #(i.e. no need to take into account old boundaries)
   post_gappei_except_for_designated_city <- pref %>%
     dplyr::filter(code != new_code) %>%
     dplyr::select(code, pop, geometry)
-  
-  
-  
+
+
+
   #Clean the data on old boundaries
   pre_gappei_geom <- old_boundary %>%
     dplyr::filter(N03_007 %in% old_code) %>%
     dplyr::select(N03_004, N03_007, geometry)
   names(pre_gappei_geom) <- c("municipality", "code", "geometry")
-  
+
   #get rid of unnecessary rows and columns
   cleaned_pop_by_old_boundary <- pop_by_old_boundary %>%
     slice(-c(1, 2, 3, 4, 5, 6, 7, 8, 9)) %>%
     select(X, X.1, X.4, X.5)
-  #column X: municipality code; X.1:shikibetsu code(9 corresponds to the old municipalities) 
+  #column X: municipality code; X.1:shikibetsu code(9 corresponds to the old municipalities)
   #column X.4: name of old municipality; X.5: population as of 2015
-  
+
   #select the data we are interested in
   pop_data <- cleaned_pop_by_old_boundary %>%
     filter(X %in% new_code) %>%
@@ -64,29 +64,29 @@ reflect_old_boundaries <- function(pref, old_boundary, pop_by_old_boundary, old_
   #obtain the last three digits of the old municipality codes
   pop_data <- pop_data %>%
     separate(municipality, into = c("a", "old_code", "b"), sep = " ") %>%
-    select(old_code, pop) 
-  
-  
+    select(old_code, pop)
+
+
   #generate functioning municipality codes
   #(i.e. add the first two digits)
-  pref_10<- toyama$code[1] %/% 1000 #prefecture_code
+  pref_10<- pref$code[1] %/% 1000 #prefecture_code
   a <- lapply(pref_10,  paste0, pop_data$old_code)
   b <- as.data.frame(a, col.names = "code")
-  
+
   #final version of 2015(2020) population based on old boundaries
   pop_data <- bind_cols(b, pop_data)
-  
-  
+
+
   #match with dataframes based on municipality code
   old_joined <- merge(pre_gappei_geom, pop_data, by = "code")
   old_joined_simp <- old_joined %>%
     select(code, pop, geometry)
   old_joined_simp$code <- as.numeric(old_joined_simp$code)
   old_joined_simp$pop <- as.numeric(old_joined_simp$pop)
-  
+
   #merge with the data that excludes the designated city
   merged <- dplyr::bind_rows(old_joined_simp, post_gappei_except_for_designated_city)
-  
+
   return(merged)
 }
 
