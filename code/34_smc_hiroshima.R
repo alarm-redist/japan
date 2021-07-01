845928b94b8621aa3b5ea785fd897eb91a25c493
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-07-01 12:36:55 +0900

    workflow for splitting municipalities (Fukuyama)

--- code/34_smc_hiroshima.R
+++ code/34_smc_hiroshima.R
@@ -92,13 +92,13 @@ saveRDS(sim_smc_pref0, paste("simulation/",
                             sep = ""))
 
 # test with map
-redist::redist.plot.plans(sim_smc_pref,
+test_map_pref0 <- redist::redist.plot.plans(sim_smc_pref0,
                           draws = 1:6,
-                          geom = pref_map) +
+                          geom = pref0_map) +
   labs(caption = "SMC")
 
 
-###############0 split analysis#############
+###############Skip(0 split analysis)#############
 simulation_weight_disparity_table(sim_smc_pref)
 #probably around 1.10
 
@@ -133,28 +133,89 @@ census2020 %>%
 #the 1st ~ 4th largest municipalities will be split.
 #江田島市 and 三原市, which are currently split, will no longer be split.
 
+pref1 <- pref
+
 # -------- 2015 小地域 data ------------#
 #First have to obtain the number of Japanese nationals per each 小地域 as of 2015
 #JINKO in pref includes foreigners too -> calculate Japanese population
-age_pops <- download_pop_demographics(pref_num) #first download data on pop by age group
-pref <- calc_kokumin(pref, age_pops)
+dem_pops <- download_pop_demographics(pref_num) #first download data
+pref1 <- calc_kokumin(pref1, dem_pops)
 
 # -------- Estimate 2020 小地域 data ------------#
 #Estimate 2020 Pop at the 小地域-level (pref is currently based on 2015 Census)
 #*Note: when splitting the 4 municipalities based on the pre-gappei boundaries,
 #*the population data will be based on the 2015 Census
 #*this inconsistency will be resolved once we get access to the 2020 Census
-pref <- estimate_2020_pop(pref, census2020)
-#The column "pop" in "pref" now indicates the "estimated number of Japanese nationals"
+pref1 <- estimate_2020_pop(pref1, census2020)
 
 # -------- Make naming consistent ------------#
-dplyr::select(code, geometry, pop)
+pref1 <- pref1 %>%
+  dplyr::rename(pop = pop_estimate) %>%
+  dplyr::select(code, geometry, pop)
 
 # -------- Merge municipalities (with exceptions) ------------#
-pref <- merge_small(pref, split_codes = c(), intact_codes = c(34101, 34102, 34103, 34104, 34105,
+pref1 <- merge_small(pref1, split_codes = 34207, intact_codes = c(34101, 34102, 34103, 34104, 34105,
                                            34106, 34107, 34108))
+#split Fukuyamashi (34207)
 #made sure to group together the 8 Wards of Hiroshima City together
-#wards are treated as single municipalities; splitting Hiroshima City doesn't count as a municipality split
+#wards are treated as single municipalities;
+#splitting Hiroshima City doesn't count as a municipality split
+
+# -------- Merge gun (0 exceptions) ------------#
+pref1 <- merge_gun(pref1, exception = 34207)
+#make sure to set Fukuyamashi as an exception; or else Fukuyamashi will be grouped together
 
 # -------- Old boundary ------------#
-pref <- reflect_old_boundaries(34)
+#first download data
+old_boundary <- download_old_shp(pref_code = pref_num)
+pop_by_old_boundary <- download_2015pop_old(pref_code = pref_num)
+
+pref1 <- reflect_old_boundaries(pref1,
+                                old_boundary = old_boundary,
+                                pop_by_old_boundary = pop_by_old_boundary,
+                                old_code = c(34207, 34481, 34482, 34501, 34524),
+                                #codes of municipalities that now belong to Fukuyamashi
+                                 new_code = 34207) #code of merged municipality (Fukuyamashi)
+
+# -------- Ferries ------------#
+edge1 <- add_ferries(pref1)
+
+# -------- set up for simulation ------------#
+# simulation parameters
+pref1adj <- redist::redist.adjacency(pref1) # Adjacency list
+#add edge
+pref1adj <- geomander::add_edge(pref1adj, edge1$V1, edge1$V2)
+
+pref1_map <- redist::redist_map(pref1,
+                                ndists = ndists_new,
+                                pop_tol= 0.08,
+                                total_pop = pop,
+                                adj = pref1adj)
+
+#save(list=ls(all=TRUE), file="34_smc_hiroshima_data1.Rdata")
+
+# --------- SMC simulation ----------------#
+# simulation
+sim_smc_pref1 <- redist::redist_smc(pref1_map,
+                                    nsims = nsims)
+
+# save it
+saveRDS(sim_smc_pref1, paste("simulation/",
+                             as.character(pref_num),
+                             "_",
+                             as.character(pref_name),
+                             "_",
+                             as.character(sim_type),
+                             "_",
+                             as.character(nsims),
+                             "_",
+                             "one split (Fukuyama)",
+                             ".Rds",
+                             sep = ""))
+
+# test with map
+test_map_pref1 <- redist::redist.plot.plans(sim_smc_pref1,
+                                            draws = 1:6,
+                                            geom = pref1_map) +
+  labs(caption = "SMC")
+
