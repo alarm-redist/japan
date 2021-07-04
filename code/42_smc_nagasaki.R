b9161ad228d3ac400c2c45ff49ef28322f770b7b
reiy24 <rei.yatsuhashi@gmail.com>
2021-07-04 21:25:02 +0900

    Update 42_smc_nagasaki.R

--- code/42_smc_nagasaki.R
+++ code/42_smc_nagasaki.R
@@ -299,6 +299,7 @@ improved_plans <- as.data.frame(
 names(improved_plans) <- c(names(wgt_smc_0), "Dissimilarity", "Splits")
 
 plot_smc <- ggplot(improved_plans, aes(Dissimilarity, LH, colour = Splits)) +
-  geom_point(size = 1)
+  geom_point(size = 1, alpha = 0.3)
 ggMarginal(plot_smc, groupColour = TRUE, groupFill = TRUE)
+
 
