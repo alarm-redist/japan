c8df39204558f36f045853ce99879bfbc958b690
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2021-07-05 00:31:00 +0900

    used for() automation

--- code/25_smc_shiga.R
+++ code/25_smc_shiga.R
@@ -52,47 +52,84 @@ for(i in 0:nsplit){
                        intact_codes = intact_codes,
                        old_code = old_code,
                        merge_gun_exception = merge_gun_exception)
+
+  #------------- set up map ----------------
+  # simulation parameters
+  prefadj <- redist::redist.adjacency(shp = pref_n) # Adjacency list
+
+  # add ferries
+  # ignore errors if there is no ferry
+  #ferries <- add_ferries(pref_n)
+  #prefadj <- geomander::add_edge(prefadj,
+  #                               ferries[, 1],
+  #                               ferries[, 2],
+  #                               zero = TRUE)
+  # check contiguity
+  #suggest <-  geomander::suggest_component_connection(shp = pref_n,
+  #                                                    adj = prefadj)
+  #prefadj <- geomander::add_edge(prefadj,
+  #                               suggest$x,
+  #                               suggest$y,
+  #                               zero = TRUE)
+
+  # define map
+  pref_map <- redist::redist_map(pref_n,
+                                 ndists = ndists_new,
+                                 pop_tol= 0.40,
+                                 total_pop = pop,
+                                 adj = prefadj)
+
+  ###### simulation ######
+  sim_smc_pref <- redist::redist_smc(pref_map,
+                                     nsims = nsims)
+  # save it
+  saveRDS(sim_smc_pref, paste("simulation/",
+                              as.character(pref_code),
+                              "_",
+                              as.character(pref_name),
+                              "_",
+                              as.character(sim_type),
+                              "_",
+                              as.character(nsims),
+                              "_",
+                              as.character(i),
+                              "split.Rds",
+                              sep = ""))
+
+  # get plans
+  smc_plans_pref <- redist::get_plans_matrix(sim_smc_pref)
+
+  # get disparity data
+  smc_weight_pref <- simulation_weight_disparity_table(sim_smc_pref)
+
+  # rename elements to be used
   assign(paste("pref_", i, sep = ""),
          pref_n)
+  assign(paste("prefadj_", i, sep = ""),
+         prefadj)
+  assign(paste("pref_map_", i, sep = ""),
+         pref_map)
+  assign(paste("sim_smc_pref_", i, sep = ""),
+         sim_smc_pref)
+  assign(paste("smc_plans_pref_", i, sep = ""),
+         smc_plans_pref)
+  assign(paste("smc_weight_pref_", i, sep = ""),
+         smc_weight_pref)
+
+  rm(list= ls()[(ls() %in% c("pref_n",
+                             "prefadj",
+                             "pref_map",
+                             "sim_smc_pref",
+                             "smc_plans_pref",
+                             "smc_weight_pref")
+                 )])
+
 }
 
-# -------- set up for simulation ------------#
-# simulation parameters
-pref0adj <- redist::redist.adjacency(pref0) # Adjacency list
-#add edge
-pref0adj <- geomander::add_edge(pref0adj, edge0$V1, edge0$V2)
-
-pref0_map <- redist::redist_map(pref0,
-                                ndists = ndists_new,
-                                pop_tol= 0.3,
-                                total_pop = pop,
-                                adj = pref0adj)
 
 
-# --------- SMC simulation ----------------#
-# simulation
-sim_smc_pref0 <- redist::redist_smc(pref0_map,
-                                    nsims = nsims)
 
-# save it
-saveRDS(sim_smc_pref0, paste("simulation/",
-                             as.character(pref_code),
-                             "_",
-                             as.character(pref_name),
-                             "_",
-                             as.character(sim_type),
-                             "_",
-                             as.character(nsims),
-                             "_",
-                             as.character(nsplit),
-                             "split.Rds",
-                             sep = ""))
 
-# test with map
-redist::redist.plot.plans(sim_smc_pref0,
-                          draws = 1:6,
-                          geom = pref0_map) +
-  labs(caption = sim_type)
 
 # -------- Evaluating Redistricting Plan (0 split)------------#
 # get plans
@@ -162,102 +199,7 @@ saveRDS(optimal_map, paste("plots/",
                            sep = ""))
 
 ###############Workflow for 1 split##################
-# additional set up
-nsplit <- 1
-seirei_shitei <- c(0)
-intact_codes <- c(0)
-merge_gun_exception <- c(0)
-old_code <- c(25201, 25301)
-# -------- Choose which city to split ------------#
-# rank without seirei shitei (government designated) city
-# choose the city to split
-split_codes <- census2020 %>%
-  dplyr::filter(code > pref_code*1000 &
-                  code < (pref_code + 1)*1000 &
-                  code != seirei_shitei) %>%
-  dplyr::arrange(desc(pop_national)) %>%
-  dplyr::select(code) %>%
-  dplyr::slice(nsplit) %>%
-  tibble::deframe()
 
-# -------- 2015 小地域 data ------------#
-#First have to obtain the number of Japanese nationals per each 小地域 as of 2015
-#JINKO in pref includes foreigners too -> calculate Japanese population
-pref1 <- pref
-dem_pops <- download_pop_demographics(pref_code) #first download data
-pref1 <- calc_kokumin(pref1, dem_pops)
-
-# -------- Estimate 2020 小地域 data ------------#
-#Estimate 2020 Pop at the 小地域-level (pref is currently based on 2015 Census)
-#*Note: when splitting the 4 municipalities based on the pre-gappei boundaries,
-#*the population data will be based on the 2015 Census
-#*this inconsistency will be resolved once we get access to the 2020 Census
-pref1 <- estimate_2020_pop(pref1, census2020)
-
-# -------- Make naming consistent ------------#
-pref1 <- pref1 %>%
-  dplyr::rename(pop = pop_estimate) %>%
-  dplyr::select(code, geometry, pop)
-
-# -------- Merge municipalities (with exceptions) ------------#
-pref1 <- merge_small(pref = pref1,
-                     split_codes = split_codes,
-                     intact_codes = intact_codes)
-
-# -------- Merge gun (0 exceptions) ------------#
-pref1 <- merge_gun(pref = pref1,
-                   exception = merge_gun_exception)
-
-# -------- Old boundary ------------#
-#first download data
-old_boundary <- download_old_shp(pref_code = pref_code)
-pop_by_old_boundary <- download_2015pop_old(pref_code = pref_code)
-
-pref1 <- reflect_old_boundaries(pref1,
-                                old_boundary = old_boundary,
-                                pop_by_old_boundary = pop_by_old_boundary,
-                                old_code = old_code,
-                                new_code = split_codes)
-
-# -------- Ferries ------------#
-edge1 <- add_ferries(pref1)
-
-# -------- set up for simulation ------------#
-# simulation parameters
-pref1adj <- redist::redist.adjacency(pref1) # Adjacency list
-#add edge
-pref1adj <- geomander::add_edge(pref1adj, edge1$V1, edge1$V2)
-
-# create map
-pref1_map <- redist::redist_map(pref1,
-                                ndists = ndists_new,
-                                pop_tol= 0.35,
-                                total_pop = pop,
-                                adj = pref1adj)
-
-# --------- SMC simulation ----------------#
-# simulation
-sim_smc_pref1 <- redist::redist_smc(pref1_map,
-                                    nsims = nsims)
-
-# save it
-saveRDS(sim_smc_pref1, paste("simulation/",
-                             as.character(pref_code),
-                             "_",
-                             as.character(pref_name),
-                             "_",
-                             as.character(sim_type),
-                             "_",
-                             as.character(nsims),
-                             "_",
-                             as.character(nsplit),
-                             "split.Rds",
-                             sep = ""))
-
-# test with map
-redist::redist.plot.plans(sim_smc_pref1,
-                          draws = 1:6,
-                          geom = pref1_map)
 # -------- Evaluating Redistricting Plan (1 split)------------#
 # get plans
 pref1_smc_plans <- redist::get_plans_matrix(sim_smc_pref1)
--- code/xx_smc_pref.R
+++ code/xx_smc_pref.R
@@ -52,111 +52,76 @@ for(i in 0:nsplit){
                        intact_codes = intact_codes,
                        old_code = old_code,
                        merge_gun_exception = merge_gun_exception)
+
+  #------------- set up map ----------------
+  # simulation parameters
+  prefadj <- redist::redist.adjacency(shp = pref_n) # Adjacency list
+
+  # add ferries
+  # ignore errors if there is no ferry
+  #ferries <- add_ferries(pref_n)
+  #prefadj <- geomander::add_edge(prefadj,
+  #                               ferries[, 1],
+  #                               ferries[, 2],
+  #                               zero = TRUE)
+  # check contiguity
+  #suggest <-  geomander::suggest_component_connection(shp = pref_n,
+  #                                                    adj = prefadj)
+  #prefadj <- geomander::add_edge(prefadj,
+  #                               suggest$x,
+  #                               suggest$y,
+  #                               zero = TRUE)
+
+  # define map
+  pref_map <- redist::redist_map(pref_n,
+                                 ndists = ndists_new,
+                                 pop_tol= 0.40,
+                                 total_pop = pop,
+                                 adj = prefadj)
+
+  ###### simulation ######
+  sim_smc_pref <- redist::redist_smc(pref_map,
+                                     nsims = nsims)
+  # save it
+  saveRDS(sim_smc_pref, paste("simulation/",
+                              as.character(pref_code),
+                              "_",
+                              as.character(pref_name),
+                              "_",
+                              as.character(sim_type),
+                              "_",
+                              as.character(nsims),
+                              "_",
+                              as.character(i),
+                              "split.Rds",
+                              sep = ""))
+
+  # get plans
+  smc_plans_pref <- redist::get_plans_matrix(sim_smc_pref)
+
+  # get disparity data
+  smc_weight_pref <- simulation_weight_disparity_table(sim_smc_pref)
+
+  # rename elements to be used
   assign(paste("pref_", i, sep = ""),
          pref_n)
+  assign(paste("prefadj_", i, sep = ""),
+         prefadj)
+  assign(paste("pref_map_", i, sep = ""),
+         pref_map)
+  assign(paste("sim_smc_pref_", i, sep = ""),
+         sim_smc_pref)
+  assign(paste("smc_plans_pref_", i, sep = ""),
+         smc_plans_pref)
+  assign(paste("smc_weight_pref_", i, sep = ""),
+         smc_weight_pref)
+
+  rm(list= ls()[(ls() %in% c("pref_n",
+                             "prefadj",
+                             "pref_map",
+                             "sim_smc_pref",
+                             "smc_plans_pref",
+                             "smc_weight_pref")
+  )])
+
 }
-
-# -------- set up for simulation ------------#
-# simulation parameters
-pref0adj <- redist::redist.adjacency(pref0) # Adjacency list
-#add edge
-pref0adj <- geomander::add_edge(pref0adj, edge0$V1, edge0$V2)
-
-pref0_map <- redist::redist_map(pref0,
-                                ndists = ndists_new,
-                                pop_tol= pop_tol,
-                                total_pop = pop,
-                                adj = pref0adj)
-
-
-# --------- SMC simulation ----------------#
-# simulation
-sim_smc_pref0 <- redist::redist_smc(pref0_map,
-                                    nsims = nsims)
-
-# save it
-saveRDS(sim_smc_pref0, paste("simulation/",
-                             as.character(pref_code),
-                             "_",
-                             as.character(pref_name),
-                             "_",
-                             as.character(sim_type),
-                             "_",
-                             as.character(nsims),
-                             "_",
-                             as.character(nsplit),
-                             "split.Rds",
-                             sep = ""))
-
-# test with map
-redist::redist.plot.plans(sim_smc_pref0,
-                          draws = 1:6,
-                          geom = pref0_map) +
-  labs(caption = sim_type)
-
-# -------- Evaluating Redistricting Plan (0 split)------------#
-# get plans
-pref0_smc_plans <- redist::get_plans_matrix(sim_smc_pref0)
-
-# get disparity data
-wgt_tbl0 <- simulation_weight_disparity_table(sim_smc_pref0)
-
-#best ippyo no kakusa
-min(wgt_tbl0$max_to_min)
-
-# find optimal
-n <- c(1:nsims)
-wgt_tbl0 <- cbind(as.data.frame(n), wgt_tbl0)
-
-optimal <- wgt_tbl0$n[which(wgt_tbl0$max_to_min == min(wgt_tbl0$max_to_min))]
-
-#print optimal plan
-optimal_map <- redist::redist.plot.plans(sim_smc_pref0,
-                                         draws = optimal[1],
-                                         geom = pref0_map) +
-  labs(title = paste(pref_name,
-                     "_",
-                     sim_type,
-                     "_",
-                     nsims,
-                     "_",
-                     nsplit,
-                     "split_optimal plan",
-                     sep = ""),
-       subtitle = paste("Max to Min Ratio =",
-                        round(min(wgt_tbl0$max_to_min), 4),
-                        "\n Loosemore-Hanby index =",
-                        round(min(wgt_tbl0$LH), 4),
-                        sep = " "),
-       caption = paste("#",
-                       optimal[1],
-                       sep = ""))
-optimal_map
-
-# save it
-ggsave(filename = paste("plots/",
-                        as.character(pref_code),
-                        "_",
-                        as.character(pref_name),
-                        "_",
-                        as.character(sim_type),
-                        "_",
-                        as.character(nsims),
-                        "_",
-                        as.character(nsplit),
-                        "split.png",
-                        sep = ""),
-       plot = optimal_map)
-
-saveRDS(optimal_map, paste("plots/",
-                           as.character(pref_code),
-                           "_",
-                           as.character(pref_name),
-                           "_",
-                           as.character(sim_type),
-                           "_",
-                           as.character(nsims),
-                           "_",
-                           as.character(nsplit),
-                           "split.Rds",
-                           sep = ""))
