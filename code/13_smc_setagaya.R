17edd68c1c6018abd9b004d4694968a06fbdcdd5
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-09-04 14:26:13 -0400

    Setagaya optimal plan

--- code/13_smc_setagaya.R
+++ code/13_smc_setagaya.R
@@ -9,7 +9,7 @@ library(tidyverse)
 library(redist)
 
 #load
-load("13_enum_setagaya.RData")
+load("~/Desktop/ALARM Project/Tokyo Results/by_region/setagaya/13_enum_setagaya.RData")
 
 #adjacency list
 setagaya_adj <- redist::redist.adjacency(setagaya)
@@ -38,6 +38,18 @@ saveRDS(setagaya_smc, paste("simulation/",
                             "_0",
                             ".Rds",
                             sep = ""))
+
+setagaya_smc <- readRDS("~/Desktop/ALARM Project/Tokyo Results/by_region/setagaya/13_setagaya_smc_25000.Rds")
+
+#######obtain optimal plan#################
+# get disparity table data
+setagaya_smc_wgt <- simulation_weight_disparity_table(setagaya_smc)
+n <- c(1:25000)
+setagaya_smc_wgt <- cbind(n, setagaya_smc_wgt)
+setagaya_smc_wgt$n[which(setagaya_smc_wgt$max_to_min == min(setagaya_smc_wgt$max_to_min))]
+#Maxmin 1.000026 # 3310 14212
+redist::redist.plot.plans(setagaya_smc, draws = 3310, geom = setagaya_map)
+
 
 
 
