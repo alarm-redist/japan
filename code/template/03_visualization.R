db0c713f4df34bd92337588e04f3d583ecfde85a
Rei Yatsuhashi <rei.yatsuhashi@gmail.com>
2021-11-30 15:39:47 -0500

    Update 03_visualization.R

--- code/template/03_visualization.R
+++ code/template/03_visualization.R
@@ -11,6 +11,22 @@ koiki <- c(a_koiki, b_koiki, c_koiki)
 
 for (i in 0:n_split)
 {
-  pref_smc_plans_n <- redist::get_plans_matrix(sim_smc_pref_0)
+  sim_smc_pref_n <- readRDS(paste("data-out/plans/",
+                                  as.character(pref_code),
+                                  "_",
+                                  as.character(pref_name),
+                                  "_",
+                                  as.character(sim_type),
+                                  "_",
+                                  as.character(nsims),
+                                  "_",
+                                  as.character(i),
+                                  ".Rds",
+                                  sep = ""), refhook = NULL)
+  pref_smc_plans_n <- redist::get_plans_matrix(sim_smc_pref_n)
+  
+  assign(paste("pref_smc_plans_", i, sep = ""), pref_smc_plans_n)
+  
 }
+
 
