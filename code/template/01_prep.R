513e4ae3e64c0270f1b8b6628e267dbb446463d6
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2021-12-20 15:06:22 -0500

    download data

--- code/template/01_prep.R
+++ code/template/01_prep.R
@@ -28,25 +28,22 @@ sim_type <- "smc"
 nsims <- 25000
 pref_code <- 0
 pref_name <- ""
-lakes_removed <- c() 
+lakes_removed <- c()
 ndists_new <- 0
 ndists_old <- 0
 n_split <- 0
 
 # Change time limit
-options(timeout = 300) 
+options(timeout = 300)
 
-# Download census shapefile
+# Download 2015 Census shapefile
 pref_raw <- download_shp(pref_code)
+# Download 2015 population data: Only for urban prefectures
 dem_pops <- download_pop_demographics(pref_code)
 
 # Download and clean 2020 census data
-total <- download_2020_census(type = "total")
-foreigner <- download_2020_census(type = "foreigner")
-census2020 <- clean_2020_census(total = total, foreigner = foreigner)
+census2020 <- clean_2020_census(pref_code)
 
-# Download data from old boundaries (pre-平成の大合併)
-old_boundary <- download_old_shp(pref_code = pref_code)
+# Download data from old boundaries (pre-平成の大合併): Only for rural prefectures
+old_boundary <- download_old_shp(pref_code)
 
-# Populations based on historical boundaries
-pop_by_old_boundary <- download_2015pop_old(pref_code = pref_code)
