ffb15f465ab9d4ae06e2182f2635f05f4dd78d2d
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2022-01-11 15:57:15 -0500

    Tokyo test

---
+++ code/13_tokyo_01_prep.R
@@ -0,0 +1,49 @@
+###############################################################################
+# Download and prepare data for Tokyo analysis
+# © ALARM Project, November 2021
+###############################################################################
+
+suppressMessages({
+  library(dplyr)
+  library(readr)
+  library(sf)
+  library(redist)
+  library(geomander)
+  library(cli)
+  library(here)
+  library(tidyverse)
+  library(nngeo)
+  devtools::load_all() # load utilities
+})
+
+set.seed(12345)
+
+# Pull functions
+setwd("R")
+files.sources = list.files()
+sapply(files.sources, source)
+setwd("..")
+
+# TODO: Define parameters for simulation
+sim_type <- "smc"
+nsims <- 25000
+pref_code <- 13
+pref_name <- "tokyo"
+lakes_removed <- c()
+ndists_new <- 30
+ndists_old <- 25
+
+# Code of 郡 that are split under the status quo
+gun_exception <- c()
+
+# Change time limit
+options(timeout = 300)
+
+# Download 2015 Census shapefile
+pref_raw <- download_shp(pref_code)
+# Download 2015 population data: Only for urban prefectures
+dem_pops <- download_pop_demographics(pref_code)
+
+# Download and clean 2020 census data
+census2020 <- clean_2020_census(pref_code)
+
