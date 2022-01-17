9766dace4be35c250061269d5d30028c2166f29f
Sho Miyazaki <70427938+shomiyazaki2000@users.noreply.github.com>
2022-01-17 21:00:36 +0900

    added vs Tottori2

--- code/template/01_prep.R
+++ code/template/01_prep.R
@@ -32,7 +32,8 @@ pref_name <- ""
 lakes_removed <- c()
 ndists_new <- 0
 ndists_old <- 0
-sq_maxmin <- 1
+sq_max_min <- 1
+sq_max_min_vs_tottori2 <- 1
 sq_splits <- 0
 
 # Code of 郡 that are split under the status quo
