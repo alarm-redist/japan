413965d7cd5fddc6b6284c1a1cde1af625029230
shomiyazaki2000 <sho.miyazaki.2000@gmail.com>
2022-01-30 17:19:04 +0900

    fixed the function

--- R/status_quo_match.R
+++ R/status_quo_match.R
@@ -1,6 +1,6 @@
 #' Match current districts
 #'
-#' @param pref sf object of cleaned, collated, census data for a prefecture
+#' @param pref_raw sf object of cleaned, collated, census data for a prefecture
 #'
 #' @return sf object with current legislative district code added
 #'
@@ -11,16 +11,25 @@
 #' @export
 #'
 
-status_quo_match <- function(pref){
+status_quo_match <- function(pref_raw){
 
   # initialize object
-  pref_ku <- pref
+  pref_ku <- pref_raw
 
   # find the prefecture name
-  pref_num <- floor(pref$code[1]/1000)
+  pref_num <- floor(as.numeric(pref_raw$KEY_CODE[1])/10000000)
 
   # import district data
-  load(file = "./data-raw/district_data.rda")
+  download.file('https://home.csis.u-tokyo.ac.jp/~nishizawa/senkyoku/senkyoku289polygon_detailed.zip', 'data-raw/senkyoku289polygon_detailed.zip')
+
+  # unzip the downloaded zip file
+  unzip("data-raw/senkyoku289polygon_detailed.zip", exdir = "data-raw/")
+
+  # return the shp file
+  district_data <- sf::st_read('data-raw/senkyoku289polygon_detailed/senkyoku289polygon_detailed.shp')
+
+  # remove the zip file after zip is decompressed
+  file.remove("data-raw/senkyoku289polygon_detailed.zip")
 
   # subset to the interested prefecture
   district_data <- district_data %>%
@@ -29,10 +38,10 @@ status_quo_match <- function(pref){
   district_data <- sf::st_make_valid(district_data)
 
   # re-projecting sf_district to match pref's CRS.
-  sf::st_crs(district_data) <- sf::st_crs(pref)
+  sf::st_crs(district_data) <- sf::st_crs(pref_raw)
 
   district_data <- district_data[which(sf::st_is_empty(district_data) != 1), ]
-  pref_points <- sf::st_centroid(pref)
+  pref_points <- sf::st_centroid(pref_raw)
 
   # find intersection to label the districts
   ku <- district_data[unlist(nngeo::st_nn(pref_points, district_data)), ]$ku
