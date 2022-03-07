6c132908e458dd68f4ccb33180a171493bc9f7b8
kentoyamada100 <84626919+kentoyamada100@users.noreply.github.com>
2022-03-06 21:53:46 -0500

    Shift-JIS -> cp932 (fix pop. data)

--- R/clean_pref_pop_2020.R
+++ R/clean_pref_pop_2020.R
@@ -9,28 +9,45 @@
 #' @export
 #'
 
-clean_pref_pop_2020 <- function(pref_pop_2020){
+clean_pref_pop_2020 <- function(pref_pop_2020, sub_code = FALSE){
 
   pref_pop_2020 <- pref_pop_2020 %>%
 
     # Select and rename columns
-    select(V2, V3, V4, V12, V10, V15) %>%
-    rename(mun_code = V2, sub_code = V3, sub_name = V10,
-           level_code = V4, total_2020 = V12, foreign_2020 = V15) %>%
-    mutate(total_2020 = as.numeric(total_2020),
-           foreign_2020 = as.numeric(foreign_2020)) %>%
+    select(X2, X3, X4, X12, X10, X15) %>%
+    rename(mun_code = X2, sub_code = X3, sub_name = X10,
+           level_code = X4, total_2020 = X12, foreign_2020 = X15) %>%
+    mutate(mun_code = as.numeric(mun_code),
+           sub_code = as.numeric(sub_code),
+           total_2020 = as.numeric(total_2020),
+           foreign_2020 = as.numeric(foreign_2020))
 
-    # Discard aggregated/disaggregated data
-    filter(level_code %in% c("1", "4") == FALSE) %>%
+  if(sub_code == TRUE){ # Returns population data at the 小地域 level
+
+    pref_pop_2020 <- pref_pop_2020 %>%
+
+      # Discard aggregated/disaggregated data and keep data at 小地域 level
+      filter(level_code %in% c("1", "4") == FALSE)
+
+
+  } else { # Returns population data ath the municipality level
+
+    pref_pop_2020 <- pref_pop_2020 %>%
+
+      # Filter out municipality level data
+      filter(level_code  == "1")
+  }
+
+  pref_pop_2020 <- pref_pop_2020 %>%
 
     # Replace NA's with 0
     mutate(total_2020 = ifelse(is.na(total_2020), 0, total_2020),
            foreign_2020 = ifelse(is.na(foreign_2020), 0, foreign_2020),
            pop = total_2020 - foreign_2020) %>%
+
     select(mun_code, sub_code, sub_name, pop)
 
   pref_pop_2020 <- as_tibble(pref_pop_2020)
 
   return(pref_pop_2020)
-
 }
