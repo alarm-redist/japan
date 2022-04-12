04a48bf4ad45161e78b5a1b5c71f65eb47800806
Ubuntu <kentoyamada@yamada-kento-redist-3.c0h1hy15qikevlclovip4sa0ag.bx.internal.cloudapp.net>
2022-04-12 02:21:01 +0000

    Miyagi 01-02 script

--- analyses/04_miyagi/02_sim_04_miyagi.R
+++ analyses/04_miyagi/02_sim_04_miyagi.R
@@ -1,8 +1,9 @@
 ###############################################################################
 # Simulations for `04_Miyagi`
-# © ALARM Project, March 2021
+# © ALARM Project, April 2021
 ###############################################################################
 
+####-------------- 1. Method for Rural Prefectures-------------------------####
 # Add information about 郡
 pref <- merge_gun(pref)
 
@@ -40,6 +41,20 @@ pref_1 <- reflect_old_boundaries(pref_0, old_mun, census_mun_old_2020, new_1)
 prefadj_0 <- redist::redist.adjacency(pref_0)
 prefadj_1 <- redist::redist.adjacency(pref_1)
 
+# Optional: Suggest connection between disconnected groups
+"suggest <-  geomander::suggest_component_connection(shp = pref_n,
+                                                    adj = prefadj_n)
+prefadj_n <- geomander::add_edge(prefadj_n,
+                                 suggest$x,
+                                 suggest$y,
+                                 zero = TRUE)"
+
+
+# TODO Repair adjacencies if necessary, and document these changes.
+# prefadj_x <- geomander::add_edge(prefadj_x,
+# which(pref_x$pre_gappei_code == xxxxx),
+# which(pref_x$pre_gappei_code == xxxxx))
+
 # Run simulations
 run_simulations <- function(pref_n, prefadj_n){
 
@@ -53,7 +68,7 @@ run_simulations <- function(pref_n, prefadj_n){
   # Create redist.map object
   pref_map_n <- redist::redist_map(pref_n,
                                    ndists = ndists_new,
-                                   pop_tol= 0.08,
+                                   pop_tol= pop_tol,
                                    total_pop = pop,
                                    adj = prefadj_n)
 
@@ -70,8 +85,6 @@ run_simulations <- function(pref_n, prefadj_n){
                         "_",
                         as.character(pref_name),
                         "_",
-                        as.character(nsims),
-                        "_",
                         as.character(i),
                         ".Rds",
                         sep = ""))
@@ -80,23 +93,21 @@ run_simulations <- function(pref_n, prefadj_n){
                            as.character(pref_code),
                            "_",
                            as.character(pref_name),
-                           "_",
-                           as.character(nsims),
                            "_adj_",
                            as.character(i),
                            ".Rds",
                            sep = ""))
 
-  saveRDS(pref_map_n, paste("data-out/maps/",
-                            as.character(pref_code),
-                            "_",
-                            as.character(pref_name),
-                            "_map_",
-                            as.character(nsims),
-                            "_",
-                            as.character(i),
-                            ".Rds",
-                            sep = ""))
+  # pref_map object: to be uploaded to Dataverse
+  write_rds(pref_map_n, paste("data-out/maps/",
+                              as.character(pref_code),
+                              "_",
+                              as.character(pref_name),
+                              "_hr_2020_map_",
+                              as.character(i),
+                              ".rds",
+                              sep = ""),
+            compress = "xz")
 
   saveRDS(sim_smc_pref_n, paste("data-out/plans/",
                                 as.character(pref_code),
@@ -131,3 +142,12 @@ run_simulations <- function(pref_n, prefadj_n){
 
 run_simulations(pref_0, prefadj_0)
 run_simulations(pref_1, prefadj_1)
+
+# Histogram showing plans diversity
+# Ideally, the majority of mass to would be above 50% and
+# we would not see a large spike at 0.
+# However, for some prefectures, it is impossible to get a diverse set of plans
+# because there are fewer possible plans.
+
+hist(plans_diversity(sim_smc_pref_0))
+hist(plans_diversity(sim_smc_pref_1))
