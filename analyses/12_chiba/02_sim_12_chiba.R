ab6721fced6de5a6661427ff3bc0b58ccea3bc31
shomiyazaki2000 <sho.miyazaki.2000@gmail.com>
2022-05-30 01:45:41 +0000

    fixed adjacency

--- analyses/12_chiba/02_sim_12_chiba.R
+++ analyses/12_chiba/02_sim_12_chiba.R
@@ -3,174 +3,6 @@
 # © ALARM Project, April 2021
 ###############################################################################
 
-####-------------- 1. Method for Rural Prefectures-------------------------####
-# Add information about 郡
-pref <- merge_gun(pref)
-
-# Define pref_0
-pref_0 <-  sf::st_as_sf(
-  dplyr::bind_rows(
-
-    # Set aside gun that are not respected under the status quo
-    pref %>% filter(gun_code %in% as.numeric(gun_exception)),
-
-    # Merge gun
-    pref %>%
-      dplyr::filter(gun_code %in% gun_exception == FALSE) %>%
-      dplyr::group_by(gun_code) %>%
-      dplyr::summarize(geometry = sf::st_union(geometry),
-                       pop = sum(pop),
-                       code = code[1])
-  )
-)
-
-# Define pref_1: Split largest municipality
-# Select the municipalities with the largest population (excluding the 区 of 政令指定都市)
-split_code <- (pref %>%
-                dplyr::filter(code >=
-                               (pref$code[1]%/%1000)*1000+200))[order(-(pref %>%
-                                                                        dplyr::filter(code >=
-                                                                                      (pref$code[1]%/%1000)*1000+200))$pop), ]$code[1]
-new_1 <- as.character(split_code)
-# Note that the size of Japanese population in the object census_mun_old_2020 is defined differently
-# reflect_old_boundaries() automatically estimates the size of the Japanese population
-# based on the official definition (total population - foreign population)
-pref_1 <- reflect_old_boundaries(pref_0, old_mun, census_mun_old_2020, new_1)
-
-# Add adjacency
-add_adjacency <- function(pref_n){
-
-  prefadj_n <- redist::redist.adjacency(pref_n)
-
-  # Modify according to ferry adjacencies
-  if(check_ferries(pref_code) == TRUE){
-    # add ferries
-    ferries_n <- add_ferries(pref_n)
-    prefadj_n <- geomander::add_edge(prefadj_n,
-                                     ferries_n[, 1],
-                                     ferries_n[, 2],
-                                     zero = TRUE)
-  }
-
-  #return result
-  return(prefadj_n)
-}
-
-# Make adjacency list
-prefadj_0 <- add_adjacency(pref_0)
-prefadj_1 <- add_adjacency(pref_1)
-
-# Optional: Suggest connection between disconnected groups
-"suggest <-  geomander::suggest_component_connection(shp = pref_n,
-                                                    adj = prefadj_n)
-prefadj_n <- geomander::add_edge(prefadj_n,
-                                 suggest$x,
-                                 suggest$y,
-                                 zero = TRUE)"
-
-
-# TODO Repair adjacencies if necessary, and document these changes.
-# prefadj_x <- geomander::add_edge(prefadj_x,
-                                 # which(pref_x$pre_gappei_code == xxxxx),
-                                 # which(pref_x$pre_gappei_code == xxxxx))
-
-# Run simulations
-run_simulations <- function(pref_n, prefadj_n){
-
-  # 0 split or 1 split
-  if("pre_gappei_code" %in% colnames(pref_n)){
-    i <- 1
-  }else{
-    i <- 0
-  }
-
-  # Create redist.map object
-  pref_map_n <- redist::redist_map(pref_n,
-                                   ndists = ndists_new,
-                                   pop_tol= pop_tol,
-                                   total_pop = pop,
-                                   adj = prefadj_n)
-
-  # Run simulation
-  sim_smc_pref_n <- redist::redist_smc(
-    map = pref_map_n,
-    nsims = nsims,
-    pop_temper = 0.05
-  )
-
-  # Save pref object, pref_map object, adjacency list, and simulation data
-  saveRDS(pref_n, paste("data-out/pref/",
-                        as.character(pref_code),
-                        "_",
-                        as.character(pref_name),
-                        "_",
-                        as.character(i),
-                        ".Rds",
-                        sep = ""))
-
-  saveRDS(prefadj_n, paste("data-out/pref/",
-                           as.character(pref_code),
-                           "_",
-                           as.character(pref_name),
-                           "_adj_",
-                           as.character(i),
-                           ".Rds",
-                           sep = ""))
-
-  # pref_map object: to be uploaded to Dataverse
-  write_rds(pref_map_n, paste("data-out/maps/",
-                              as.character(pref_code),
-                              "_",
-                              as.character(pref_name),
-                              "_hr_2020_map_",
-                              as.character(i),
-                              ".rds",
-                              sep = ""),
-                              compress = "xz")
-
-  saveRDS(sim_smc_pref_n, paste("data-out/plans/",
-                                as.character(pref_code),
-                                "_",
-                                as.character(pref_name),
-                                "_",
-                                as.character(sim_type),
-                                "_",
-                                as.character(nsims),
-                                "_",
-                                as.character(i),
-                                ".Rds",
-                                sep = ""))
-
-  assign(paste("pref", i, sep = "_"),
-         pref_n,
-         envir = .GlobalEnv)
-
-  assign(paste("pref", "map", i, sep = "_"),
-         pref_map_n,
-         envir = .GlobalEnv)
-
-  assign(paste("prefadj", i, sep = "_"),
-         prefadj_n,
-         envir = .GlobalEnv)
-
-  assign(paste("sim", "smc", "pref", i, sep = "_"),
-         sim_smc_pref_n,
-         envir = .GlobalEnv)
-
-}
-
-run_simulations(pref_0, prefadj_0)
-run_simulations(pref_1, prefadj_1)
-
-# Histogram showing plans diversity
-# Ideally, the majority of mass to would be above 50% and
-# we would not see a large spike at 0.
-# However, for some prefectures, it is impossible to get a diverse set of plans
-# because there are fewer possible plans.
-
-hist(plans_diversity(sim_smc_pref_0))
-hist(plans_diversity(sim_smc_pref_1))
-
 ####-------------- 2. Method for Urban Prefectures-------------------------####
 # Obtain codes of 郡 to merge
 pref <- merge_gun(pref)
@@ -244,34 +76,67 @@ if(check_ferries(pref_code) == TRUE){
 }
 
 # Optional: Suggest connection between disconnected groups
-"suggest <-  geomander::suggest_component_connection(shp = pref,
+suggest <-  geomander::suggest_component_connection(shp = pref,
                                                     adj = prefadj)
 prefadj <- geomander::add_edge(prefadj,
                                suggest$x,
                                suggest$y,
-                               zero = TRUE)"
+                               zero = TRUE)
 
 # TODO Repair adjacencies if necessary, and document these changes.
-# prefadj <- geomander::add_edge(prefadj,
-                                # which(pref$code == xxxxx & pref$sub_code == "xxxx"),
-                                # which(pref$code == xxxxx & pref$sub_code == "xxxx"))
+pref_add_edge <-
+  matrix(c(
+    # 印西市小林北 and 小林
+    which(pref$code == 12231 & pref$sub_code == 90),
+    which(pref$code == 12231 & pref$sub_code == 70),
+    # 印西市笠神 and its 飛地
+    which(pref$code == 12231 & pref$sub_code == 640)[1],
+    which(pref$code == 12231 & pref$sub_code == 640)[2],
+    # 鴨川市太海浜 and its 仁右衛門島
+    which(pref$code == 12223 & pref$sub_code == 580)[1],
+    which(pref$code == 12223 & pref$sub_code == 580)[2],
+    # 横芝光町栗山 and its 飛地
+    which(pref$code == 12410 & pref$sub_code == 880)[1],
+    which(pref$code == 12410 & pref$sub_code == 880)[2],
+    # 香取郡
+    which(pref$code == 12420)[1],
+    which(pref$code == 12420)[2],
+    which(pref$code == 12420)[1],
+    which(pref$code == 12420)[3],
+    which(pref$code == 12420)[1],
+    which(pref$code == 12420)[4],
+    which(pref$code == 12420)[1],
+    which(pref$code == 12420)[5],
+
+    # 安房郡鋸南町
+    which(pref$code == 12460)[1],
+    which(pref$code == 12460)[2],
+    which(pref$code == 12460)[1],
+    which(pref$code == 12460)[3]
+    ), ncol = 2, byrow = TRUE)
+
+#Add edges
+prefadj <- geomander::add_edge(prefadj,
+                               pref_add_edge[,1],
+                               pref_add_edge[,2])
 
 # Define pref_map object
 pref_map <- redist::redist_map(pref,
                                ndists = ndists_new,
-                               pop_tol= 0.10,
+                               pop_tol= 0.20,
                                total_pop = pop,
                                adj = prefadj)
 
 # Define constraints
 constr = redist::redist_constr(pref_map)
-constr = redist::add_constr_splits(constr, strength = 5, admin = pref_map$code)
-constr = redist::add_constr_multisplits(constr, strength = 10, admin = pref_map$code)
+constr = redist::add_constr_splits(constr, strength = 3, admin = pref_map$code)
+constr = redist::add_constr_multisplits(constr, strength = 6, admin = pref_map$code)
 
 # Run simulation
 sim_smc_pref <- redist::redist_smc(
   map = pref_map,
   nsims = nsims,
+  runs = 2L,
   counties = pref$code,
   constraints = constr,
   pop_temper = 0.05)
@@ -315,7 +180,7 @@ saveRDS(sim_smc_pref, paste("data-out/plans/",
                             "_",
                             as.character(sim_type),
                             "_",
-                            as.character(nsims),
+                            as.character(nsims*2),
                             ".Rds",
                             sep = ""))
 
