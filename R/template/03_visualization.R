52f5174334f4912683d8ceed1d1e73bf1dbbf3ab
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2022-05-30 11:35:35 +0900

    matrix

--- R/template/03_visualization.R
+++ R/template/03_visualization.R
@@ -84,20 +84,31 @@ koiki_2_1[koiki_2_1 %in% koiki_2_codes] <- 2
 
 # Count number of municipality splits
 num_mun_split_1 <- count_splits(pref_smc_plans_1, pref_map_1$code)
-mun_split_1 <- redist::redist.splits(pref_smc_plans_1, pref_map_1$code)
+mun_split_1 <- redist::redist.splits(pref_smc_plans_1, pref_map_1$code) %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+mun_split_1 <- mun_split_1[,1]
 
 # Count number of gun splits
-gun_split_0 <- redist::redist.splits(pref_smc_plans_0, pref_map_0$gun_code)
-gun_split_1 <- redist::redist.splits(pref_smc_plans_1, pref_map_1$gun_code)
+gun_split_0 <- redist::redist.splits(pref_smc_plans_0, pref_map_0$gun_code) %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+gun_split_0 <- gun_split_0[,1]
+gun_split_1 <- redist::redist.splits(pref_smc_plans_1, pref_map_1$gun_code) %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+gun_split_1 <- gun_split_1[,1]
 
 # Count number of koiki renkei splits
 koiki_split_0 <-
   redist::redist.splits(pref_smc_plans_0, koiki_1_0) +
   redist::redist.splits(pref_smc_plans_0, koiki_2_0)
+koiki_split_0 <- koiki_split_0 %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+koiki_split_0 <- koiki_split_0[,1]
 koiki_split_1 <-
   redist::redist.splits(pref_smc_plans_1, koiki_1_1) +
   redist::redist.splits(pref_smc_plans_1, koiki_2_1)
-
+koiki_split_1 <- koiki_split_1 %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+koiki_split_1 <- koiki_split_1[,1]
 # Compile results: 0 split
 results_0 <- data.frame(matrix(ncol = 0, nrow = nrow(wgt_smc_0)))
 results_0$max_to_min <- wgt_smc_0$max_to_min
@@ -355,19 +366,25 @@ koiki_2[!koiki_2 %in% 2] <-
 
 # Count number of municipality splits
 num_mun_split <- count_splits(pref_smc_plans, pref_map$code)
-mun_split <- redist::redist.splits(pref_smc_plans, pref_map$code)
+mun_split <- redist::redist.splits(pref_smc_plans, pref_map$code) %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+mun_split <- mun_split[,1]
 
 # Count number of gun splits
 gun_index <- pref$gun_code
 gun_index[gun_index < (pref_map$code[1]%/%1000)*1000+300] <-
   seq(100000, 100000 + length(gun_index[gun_index < (pref_map$code[1]%/%1000)*1000+300])-1, by = 1)
 
-gun_split <- redist::redist.splits(pref_smc_plans, gun_index)
-
+gun_split <- redist::redist.splits(pref_smc_plans, gun_index) %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+gun_split <- gun_split[,1]
 # Count number of koiki renkei splits
 koiki_split <-
   redist::redist.splits(pref_smc_plans, koiki_1) +
   redist::redist.splits(pref_smc_plans, koiki_2)
+koiki_split <- koiki_split %>%
+  matrix(ncol = ndists_new, byrow = TRUE)
+koiki_split <- koiki_split[,1]
 
 # Compile results
 results <- data.frame(matrix(ncol = 0, nrow = nrow(wgt_smc)))
