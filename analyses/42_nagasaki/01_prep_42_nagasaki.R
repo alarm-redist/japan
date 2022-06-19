647b68a2beea756af6c135675a2baa475d0bbc27
Ubuntu <kentoyamada@yamada-kento-redist-3.c0h1hy15qikevlclovip4sa0ag.bx.internal.cloudapp.net>
2022-06-19 07:51:45 +0000

    Nagasaki new code

--- analyses/42_nagasaki/01_prep_42_nagasaki.R
+++ analyses/42_nagasaki/01_prep_42_nagasaki.R
@@ -24,7 +24,7 @@ setwd("..")
 
 # TODO: Define parameters for simulation
 sim_type <- "smc"
-nsims <- 10000  # Set so that the number of valid plans > 5,000
+nsims <- 5000  # Set so that the number of valid plans > 5,000
 pref_code <- 42
 pref_name <- "nagasaki"
 lakes_removed <- c()
