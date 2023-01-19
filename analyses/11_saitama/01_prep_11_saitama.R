92956d969d0379d1fdaa8f803df31cb44f739b2c
Sho Miyazaki <sho.miyazaki.2000@gmail.com>
2023-01-19 14:04:59 +0000

    remove the older part

--- analyses/11_saitama/01_prep_11_saitama.R
+++ analyses/11_saitama/01_prep_11_saitama.R
@@ -47,17 +47,11 @@ gun_exception <- c(11320) # Iruma (11324, 11326, 11327)
 # Change time limit
 options(timeout = 300)
 
-# Download 2015 Census shapefile
-pref_shp_2015 <- download_shp(pref_code)
-
-# Clean 2015 Census shapefile
-pref_shp_cleaned <- pref_shp_2015 %>%
+# Download and clean 2020 Census shapefile
+pref_shp_2020 <- download_shp(pref_code) %>%
     clean_jcdf()
 # Note that S_NAME shows the name of the first entry of the areas grouped
 # in the same KIHON-1 unit (i.e. disregard --丁目,字--)
-
-# Download 2020 Census data at 小地域-level
-pref_pop_2020 <- download_pop_2020(pref_code)
 
 # remove lake if needed
 "ifelse(is.null(lakes_removed),
